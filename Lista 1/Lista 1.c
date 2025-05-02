9e1a57cfafac50bccdedd697247ce731e29939e3
Gustavo Moretto <moretto.gu@gmail.com>
2025-05-01 22:23:04 -0300

    Add files via upload

---
+++ Lista 1/Lista 1.c
@@ -0,0 +1,27 @@
+1 - 	Python: “print”
+	    C: “printf”
+	    Pascal: “writeln”
+
+2 - 	Entrada: ano de nascimento e ano atual.
+	    Processamento: ano atual - ano de nascimento = idade.
+	    Saída: idade calculada.
+
+3 -     a)  Entrada: valor do salário antes do bônus e percentual de bônus.
+	        Saída: valor do salário total (base + bônus).
+	        Variáveis: salariobase, bonus, salariofinal.
+
+	    b)  Entrada: preço do litro e quantidade de litros.
+	        Saída: valor total (preço do litro * quantidade de litros)
+	        Variáveis: precolitro, qtdlitros, total.
+
+	    c)  Entrada: sexo, dia, mês e ano de nascimento, altura e peso.
+	        Saída: nulo.
+	        Variáveis: sexo, dia, mes, ano, altura, peso.
+
+4 -	    a)  precofinal = precooriginal * 1.10 - 5
+	
+    	b)  media = (n1 * p1 + n2 * p2 + n3 * p3) / (p1 + p2 + p3)
+
+    	c)  distancia = t1 * v1 + t2 * v2
+
+	    d)  y = a * x * x + b * x + c
